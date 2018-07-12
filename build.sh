0900b8b7607a17aed696922e67b4a7ba52081cfa
Zoltán Gera <gerazo@caesar.elte.hu>
2018-07-12 11:51:17 +0200

    Test with 3 toolchains

--- build.sh
+++ build.sh
@@ -12,7 +12,23 @@ GTKMM_DEPS="libgtkmm-3.0-dev"
 
 ALL_DEPS="$BUILD_DEPS $PULSEAUDIO_DEPS $GTKMM_DEPS"
 
-if [ "$1" != "-nodep" ]; then
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -nodep) NODEP=1 ;;
+    -noanal) NOANAL=1 ;;
+    -compilers) shift; COMPILERS="$1" ;;
+    *) echo "ZAMT build script parameters:"
+       echo "  -nodep                       Skip dependency detection and installation."
+       echo "  -noanal                      Skip extra analysis of sources."
+       echo "  -compilers <compiler_list>   Tests with the given compiler toolchains."
+       exit 99
+       ;;
+  esac
+  shift
+done
+
+if [ "$NODEP" != "1" ]; then
   if command -v apt >/dev/null && command -v dpkg >/dev/null ; then
     if ! dpkg -s $ALL_DEPS >/dev/null; then
       echo "\\033[1m\\033[37m\\033[42m--- Installing Dependencies (needs sudo privileges)...\\033[0m"
@@ -32,11 +48,7 @@ for COMPILER in $COMPILERS; do
       echo "\\033[1m\\033[37m\\033[42m" Creating new build setup: $BUILD_DIR "\\033[0m"
       mkdir -p $BUILD_DIR
       cd $BUILD_DIR
-      case $COMPILER in
-        gcc) CXXCOMPILER=g++ ;;
-        clang) CXXCOMPILER=clang++ ;;
-        *) CXXCOMPILER=$COMPILER ;;
-      esac
+      CXXCOMPILER=$( echo $COMPILER | sed "s/gcc/g++/g" | sed "s/clang/clang++/g" )
       CC=$COMPILER CXX=$CXXCOMPILER cmake -G "$CMAKE_PROJECT" -DCMAKE_BUILD_TYPE=$MODE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
       cd ..
     fi
@@ -63,16 +75,18 @@ for COMPILER in $COMPILERS; do
 done
 
 FAIL=0
-for MODULE in $( ls -1d */ | grep -v _build_ ); do
-  echo "\\033[1m\\033[37m\\033[42m" Analyzing module $MODULE "\\033[0m"
-  for SOURCE in $( find $MODULE"include" $MODULE"src" $MODULE"test" -regex "\(.*\.cpp\)\|\(.*\.h\)" ); do
-    if clang-format -style=file -output-replacements-xml $SOURCE | grep "<replacement " >/dev/null; then
-      echo "\\033[1m\\033[37m\\033[41m" Syntax convention problem with $SOURCE "\\033[0m"
-      clang-format -style=file $SOURCE | diff $SOURCE -
-      FAIL=3
-    fi
+if [ "$NOANAL" != "1" ]; then
+  for MODULE in $( ls -1d */ | grep -v _build_ ); do
+    echo "\\033[1m\\033[37m\\033[42m" Analyzing module $MODULE "\\033[0m"
+    for SOURCE in $( find $MODULE"include" $MODULE"src" $MODULE"test" -regex "\(.*\.cpp\)\|\(.*\.h\)" ); do
+      if clang-format -style=file -output-replacements-xml $SOURCE | grep "<replacement " >/dev/null; then
+        echo "\\033[1m\\033[37m\\033[41m" Syntax convention problem with $SOURCE "\\033[0m"
+        clang-format -style=file $SOURCE | diff $SOURCE -
+        FAIL=3
+      fi
+    done
   done
-done
+fi
 
 if [ "$FAIL" = "0" ]; then
   echo "\\033[1m\\033[37m\\033[42m Build successful. \\033[0m"
